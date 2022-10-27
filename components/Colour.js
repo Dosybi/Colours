f91c2feaec587e5161efbe9c58177ee0dfd2151f
Anton.Do <anton.do@ramotion.agency>
2022-10-27 13:01:18 +0600

    initial commit

---
+++ components/Colour.js
@@ -0,0 +1,37 @@
+import chroma from "chroma-js";
+
+const Colour = ({ colourCode, handleLock, position, icon }) => {
+  return (
+    <div
+      className={`relative flex h-1/5 w-screen flex-row justify-around md:h-screen md:flex-col ${
+        chroma(colourCode).luminance() <= 0.5 ? "text-white" : "text-black"
+      }`}
+      style={{ background: colourCode }}
+      onClick={() => {
+        navigator.clipboard.writeText(colourCode);
+      }}
+    >
+      <h2
+        className={`cursor-pointer self-center rounded-lg p-4 font-bold hover:border sm:text-base md:text-lg ${
+          chroma(colourCode).luminance() <= 0.5
+            ? "border-white"
+            : "border-black"
+        }`}
+      >
+        {colourCode}
+      </h2>
+      <div
+        className="cursor-pointer self-center"
+        onClick={(event) => handleLock(colourCode, position, event.target)}
+      >
+        {icon === "open" ? (
+          <i className="fa-solid fa-lock-open fa-xl" id="lock"></i>
+        ) : (
+          <i className="fa-solid fa-lock fa-xl" id="lock"></i>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Colour;
